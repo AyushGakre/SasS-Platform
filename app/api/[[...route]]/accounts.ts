4dae4e510e5ef0d325955f2c3658cf57f5be4bab
Ayush Gakre <122914231+AyushGakre@users.noreply.github.com>
2024-06-17 13:45:09 +0530

    tanstack

---
+++ app/api/[[...route]]/accounts.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+import { clerkMiddleware ,getAuth} from "@hono/clerk-auth";
+import db from "@/db/drizzle";
+import { accounts } from "@/db/schema";
+
+const app = new Hono()
+.get("/", 
+    clerkMiddleware(),
+    async(c)=>{
+        const auth = getAuth(c)
+        if(!auth?.userId){
+            return c.json({ error: "Unauhtorise"} ,401)
+        }
+    const data = await db.select({
+        id: accounts.id,
+        name: accounts.name
+    })
+    .from(accounts)
+    return c.json({ data })
+})
+
+export default app;
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,40 +1,15 @@
-import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { NextRequest } from 'next/server';
-import {z} from 'zod'
+
+import accounts from './accounts'
+
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app
-.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
-  })
-})
-.get("/ayush/:test", (c) => {
-    const test = c.req.param("test")
-    return c.json({
-        message : "Hello world!!",
-        test: test
-    })
-})
-.post('/post',
-    zValidator('form',
-        z.object({
-            username: z.string(),
-            password : z.number()
-        })
-    ),
-    (c)=>{
-        
-        return c.json({
-            ok : "checked"
-        })
-    }
-)
-
+const routes = app.route('/accounts', accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes;
